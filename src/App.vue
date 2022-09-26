aae14c62584a3f4985bd403a77d0dfae8c2b2d2f
CB-IT-01-1813 <cb-it-01-1813@CB-IT-01-1813s-MacBook-Pro.local>
2022-09-26 11:13:50 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+<h1 class="text-bold text-xl ml-4">Hello to my {{increment}}</h1>
+<p >how are you</p>
+<button @click="increment">Clcik me</button>
+<Tinjo title="UX Designer"/>
+<Tinjo title="Design Technologist"/>
+</template>
+<script>
+import Tinjo from './components/tinjo.vue';
+export default {
+    name: "MyComponent",
+    data() {
+        return {
+            siteName: "Learn Vue LiveLessions"
+        };
+    },
+    method: {
+        increment: () => { console.log("clicked"); }
+    },
+    components: { Tinjo }
+}
+</script>
---
+++ src/components/tinjo.vue
@@ -0,0 +1,20 @@
+<template>
+   <div class="bg-slate-100 p-4 round shadow-orange-100">
+    <h1 class="text-bold text-xl name">Hello to my app</h1>
+    <p>{{title}}</p>
+    <button>Clcik me</button>
+   </div>
+</template>
+
+<style scoped>
+    .name  {
+background: red;
+    }
+</style>
+
+<script>
+    export default {
+        name : 'TinjoComp',
+       props : ['title']
+    }
+</script>
